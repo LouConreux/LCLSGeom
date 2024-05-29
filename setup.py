52ec186c8f31f0c4d7b1c2396dc719cbee48e120
LouConreux <louis.conreux1@gmail.com>
2024-05-29 10:53:30 -0700

    [dev] Setup requirements and dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="LCLSGeom",
     version="0.1.0",
-    author="Louis Conreyx",
+    author="Louis Conreux",
     author_email="louis.conreux1@gmail.com",
     description="Miscellaneous functions for converting geometry files for LCLS geometry calibrations and optimizations.",
     long_description=open('README.md').read(),
@@ -19,4 +19,9 @@ setup(
     license="SLAC Internal Use Only",
     license_files=('LICENSE',),
     python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.20.0',
+        'pyFAI>=0.20.0',
+        'PSCalib>=0.0.0',
+    ],
 )
