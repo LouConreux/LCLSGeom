ca284bbd9a48a247154d8a0c81799cb0753c8589
LouConreux <louis.conreux1@gmail.com>
2024-11-08 10:50:57 -0800

    [func] Add functions for determining beam center and detector distance from pyFAI parameters

--- LCLSGeom/swap_geom.py
+++ LCLSGeom/swap_geom.py
@@ -27,6 +27,25 @@ DETTYPE_TO_PARS = {
     'pnccd'   : ('MTRX:V2:512:512:75:75', 'p0a0,p1a0,p2a0,p3a0'),\
 }
 
+def get_beam_center(params):
+    """
+    From pyFAI calibration parameters, return the beam center in meters
+
+    Parameters
+    ----------
+    params : list
+        Detector parameters found by PyFAI calibration
+    """
+    dist = params[0]
+    poni1 = params[1]
+    poni2 = params[2]
+    rot1 = params[3]
+    rot2 = params[4]
+    distance = dist / (np.cos(rot1) * np.cos(rot2))
+    cx = poni1 + dist * np.tan(rot2) / np.cos(rot1)
+    cy = poni2 - dist * np.tan(rot1)
+    return distance, cx, cy
+
 class ePix10k2M(Detector):
     """
     PyFAI Detector instance for the ePix10k2M
