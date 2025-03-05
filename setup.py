580244e7ef2c1bd47b762fe140da1636433ade4f
LouConreux <louis.conreux1@gmail.com>
2025-03-05 10:50:41 -0800

    [edit] Clean LCLSGeom

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/LouConreux/LCLSGeom",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    package_dir={'': 'LCLSGeom'},
+    packages=find_packages(where='LCLSGeom'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
