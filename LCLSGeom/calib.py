f2fd46feff50a3025626012cb4d78da72dd55e1f
LouConreux <louis.conreux1@gmail.com>
2025-07-10 10:08:55 -0700

    [edit] Add template .data files for mfx, cxi and mec detectors

--- LCLSGeom/calib.py
+++ LCLSGeom/calib.py
@@ -4,8 +4,10 @@ import re
 det_type_to_pars = {
     'epix10k2m': 'p0a0,p1a0,p2a0,p3a0,p4a0,p5a0,p6a0,p7a0,p8a0,p9a0,p10a0,p11a0,p12a0,p13a0,p14a0,p15a0',
     'epix10kaquad': 'p0a0,p1a0,p2a0,p3a0',
+    'jungfrau05m': 'p0a0',
     'jungfrau1m': 'p0a0,p1a0',
     'jungfrau4m': 'p0a0,p1a0,p2a0,p3a0,p4a0,p5a0,p6a0,p7a0',
+    'jungfrau16m': 'p0a0,p1a0,p2a0,p3a0,p4a0,p5a0,p6a0,p7a0,p8a0,p9a0,p10a0,p11a0,p12a0,p13a0,p14a0,p15a0',
 }
 
 calib_groups = (
