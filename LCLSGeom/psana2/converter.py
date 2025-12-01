7f436de2c54a143ee6272dd8bd5756e9fba23d2b
Louis Conreux <louis.conreux1@gmail.com>
2025-12-01 10:27:38 -0800

    MNT Clean redundant code

--- LCLSGeom/psana2/converter.py
+++ LCLSGeom/psana2/converter.py
@@ -114,23 +114,10 @@ class PsanaToPyFAI:
     def setup_detector(self):
         """
         Pass the detector segmentation and geometry info to the PyFAI detector instance
-
-        Parameters
-        ----------
-        geo : GeometryAccess
-            The GeometryAccess instance for the psana .data file
         """
+        self.detector.geo = self.geo
         self.detector.seg_geo = self.geo.get_seg_geo()
         self.detector.segname = self.geo.get_seg_geo().oname
-        top = self.geo.get_top_geo()
-        child = top.get_list_of_children()[0]
-        child.x0 = 0
-        child.y0 = 0
-        child.z0 = 0
-        child.rot_x = 0
-        child.rot_y = 0
-        child.rot_z = 0
-        self.image_geo = child
 
     def image_to_pyfai(self, x, y, z):
         """
@@ -155,19 +142,33 @@ class PsanaToPyFAI:
         pixel_index_map[..., 1] = temp_index[1]
         self.detector.pixel_index_map = pixel_index_map.astype(np.int64)
 
-    def get_image_frame_coordinates(self):
+    def get_image_frame_coordinates(self, geometry):
         """
         Return the image frame coordinates for the given geometry
+
+        Parameters
+        ----------
+        geometry : GeometryObject
+            The root geometry object defined relative to the IP.
         """
         # Set reference frame to be the image frame (i.e. no offsets or rotations)
-        x, y, z = self.image_geo.get_pixel_coords(do_tilt=True)
+        geometry.x0 = 0
+        geometry.y0 = 0
+        geometry.z0 = 0
+        geometry.rot_x = 0
+        geometry.rot_y = 0
+        geometry.rot_z = 0
+        x, y, z = geometry.get_pixel_coords(do_tilt=True)
         return x, y, z
 
     def get_pixel_corners(self):
         """
         Compute the pixel corner coordinates to instantiate a 3D PyFAI detector
         """
-        x, y, z = self.get_image_frame_coordinates()
+        geo = self.geo
+        top = geo.get_top_geo()
+        child = top.get_list_of_children()[0]
+        x, y, z = self.get_image_frame_coordinates(geometry=child)
         x, y, z = self.image_to_pyfai(x, y, z)
         npanels = self.detector.n_modules
         nasics = self.detector.n_asics
